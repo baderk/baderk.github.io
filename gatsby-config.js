2ee51bd1ceafa96ad323d0ea0f3abc0462dde0c2
baderk <baderkanawati@gmail.com>
2018-11-01 16:00:55 -0300

    added tagamanager to see if it helps

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,22 @@ module.exports = {
         cookieDomain: "baderkanawati.com",
       },
     },
+    {
+    resolve: `gatsby-plugin-google-tagmanager`,
+    options: {
+      id: "GTM-MZ2CQ8Z",
+
+      // Include GTM in development.
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
+
+      // Specify optional GTM environment details.
+      gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+      gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+    },
+  },
+
+  
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
